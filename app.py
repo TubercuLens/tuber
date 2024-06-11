90ae61f0a366acc93134290251161a5433aa38f0
TubercuLens <155637627+TubercuLens@users.noreply.github.com>
2024-06-11 21:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
